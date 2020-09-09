385a8ca1a4c4105cca2f25a71db7922bc7a250fc
yoogj0513 <yoogj0513@naver.com>
2020-09-09 23:00:59 +0900

    스프링 프로젝트 생

---
+++ src/test/java/com/yoo/SpringBootCommunityWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCommunityWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
